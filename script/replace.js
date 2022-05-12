f5aa05fb9a8326211400ad36ee5a134888e97e88
aniito <66576504+aniito@users.noreply.github.com>
2022-05-12 16:22:17 +0200

    Update replace.js

--- script/replace.js
+++ script/replace.js
@@ -1,95 +1,101 @@
-const $ = window.$;
+
+function purified(pronom) {
+  let purr = document.body.getElementsByTagName("*");
+  for (let i = 0; i < purr.length; i++) {
+      if (purr[i].nodeType == 1 || purr[i].nodeType == 3) {
+          for (let j = 0; j < purr[i].childNodes.length; j++) {
+                if (purr[i].childNodes[j].nodeType == 3) {
+                  if (purr[i].childNodes[j].nodeValue == "iel" || purr[i].childNodes[j].nodeValue== "Iel" || purr[i].childNodes[j].nodeValue== "IEl" || purr[i].childNodes[j].nodeValue== "IEL" || purr[i].childNodes[j].nodeValue== "ieL" || purr[i].childNodes[j].nodeValue== "iEl" || purr[i].childNodes[j].nodeValue== "-iel") {
+                    purr[i].childNodes[j].nodeValue = " " + pronom;
+                } else if (purr[i].childNodes[j].nodeValue == "\"IEL\"") {
+                  purr[i].childNodes[j].nodeValue = " \"" + pronom.toUpperCase() + "\"";
+                } else if (purr[i].childNodes[j].nodeValue == "\"iel\"") {
+                  purr[i].childNodes[j].nodeValue = " \"" + pronom + "\"";
+                }
+                purr[i].childNodes[j].nodeValue = purr[i].childNodes[j].nodeValue.replace(/\x20+(iel|IEL|Iel|ieL|iEl|IeL)/g, " " + pronom).replace(/^(iel|IEL|Iel|ieL|iEl|IeL)/g, " " + pronom).replace(/\x20+(("|')(iel|IEL|Iel|ieL|iEl|IeL)("|'))/g, " \"" + pronom + "\" ");
+                }
+          }
+      }
+
+  }
+      
+
+};
 
 
-  function purified(pronom) {
-    $('body :not(script):not(style)').contents().filter(function() {
-      return this.nodeType === 3;
-    }).replaceWith(function() {
-        if(this.nodeValue == "iel" || this.nodeValue == "Iel" || this.nodeValue == "IEl" || this.nodeValue == "IEL" || this.nodeValue == "ieL" || this.nodeValue == "iEl" || this.nodeValue == "-iel"){
-            return " " + pronom;
+
+function purifiedEcrit(pronom) {
+    let purr = document.body.getElementsByTagName("*");
+    for (let i = 0; i < purr.length; i++) {
+        if (purr[i].nodeType == 1 || purr[i].nodeType == 3) {
+            for (let j = 0; j < purr[i].childNodes.length; j++) {
+                if (purr[i].childNodes[j].nodeType == 3) {
+                    if (pronom == "il") {
+                        purr[i].childNodes[j].nodeValue = purr[i].childNodes[j].nodeValue.replace(/é(\.|·|\/)e /g, "é ").replace(/(\.|·|\/)e(\.|·|\/)s/g, "s").replace(/e(\.|·|\/)e/g, "e").replace(/(\.|·|\/)rice(\.|·|\/)s/g, "s").replace(/un(\.|·|\/)e/g, "un").replace(/(\.|·|\/)e /g, " ").replace(/(\.|·|\/)e$/g, " ").replace(/(\.|·|\/)fe(\.|·|\/)s/g, "s ").replace("🏳️‍🌈", "🏳️‍🌈⃠");
+                    } else {
+
+                      purr[i].childNodes[j].nodeValue = purr[i].childNodes[j].nodeValue.replace(/é(\.|·|\/)e /g, "ée ").replace(/(\.|·|\/)e(\.|·|\/)s/g, "es").replace(/e(\.|·|\/)e/g, "e").replace(/(eur| )(\.|·|\/)rice(\.|·|\/)s/g, "rices").replace(/un(\.|·|\/)e/g, "une").replace(/(\.|·|\/)e /g, "e ").replace(/(\.|·|\/)e$/g, "e ").replace(/(\.|·|\/)fe(\.|·|\/)s/g, "fes ").replace("🏳️‍🌈", "🏳️‍🌈⃠");
+                    }
+
+                    
+                }
+            }
         }
-        else if(this.nodeValue == "\"IEL\""){
-            return " \""+pronom.toUpperCase()+"\"";
-        }else if(this.nodeValue == "\"iel\""){
-          return " \""+pronom+"\"";
-      }
-      return this.nodeValue.replace(/\x20+(iel|IEL|Iel|ieL|iEl|IeL)/g, " "+pronom).replace(/^(iel|IEL|Iel|ieL|iEl|IeL)/g, " "+pronom).replace(/\x20+(("|')(iel|IEL|Iel|ieL|iEl|IeL)("|'))/g, " \"" + pronom + "\" ");
-    
-    
-    });
-  
-    
+
+    }
+
+
+
+}
+
+
+function allonsycamarades() {
+  var time = 100;
+  if(window.location.href.includes('twitter.com')){
+    time = 5500;
   }
+  setTimeout(function()
+    {
+    browser.storage.local.get("protection").then((onouoff) => {
+        if (onouoff.protection == undefined) {
 
-  function purifiedEcrit(pronom){
-    $('body :not(script):not(style)').contents().filter(function() {
-      return this.nodeType === 3;
-    }).replaceWith(function() {
-      if (pronom == "il"){
-      return this.nodeValue.replace(/é(\.|·|\/)e /g, "é ").replace(/(\.|·|\/)e(\.|·|\/)s/g, "s").replace(/e(\.|·|\/)e/g, "e").replace(/(\.|·|\/)rice(\.|·|\/)s/g, "s").replace(/un(\.|·|\/)e/g, "un").replace(/(\.|·|\/)e /g, " ").replace(/(\.|·|\/)e$/g, " ").replace(/(\.|·|\/)fe(\.|·|\/)s/g, "s ");
-      }else{
-      
-      return this.nodeValue.replace(/é(\.|·|\/)e /g, "ée ").replace(/(\.|·|\/)e(\.|·|\/)s/g, "es").replace(/e(\.|·|\/)e/g, "e").replace(/(eur| )(\.|·|\/)rice(\.|·|\/)s/g, "rices").replace(/un(\.|·|\/)e/g, "une").replace(/(\.|·|\/)e /g, "e ").replace(/(\.|·|\/)e$/g, "e ").replace(/(\.|·|\/)fe(\.|·|\/)s/g, "fes ");
+            browser.storage.local.set({"protection": "off"});
 
-      }
-    
+        } else if (onouoff.protection == "on") {
+
+            browser.storage.local.get("pronom").then((result) => {
+                if (result.pronom == undefined) {
+                    browser.storage.local.set({"pronom": "il"});
+                    purified("il");
+
+                    browser.storage.local.get("purification").then((res) => {
+                        if (res.purification == "on" || res.purification == undefined) {
+                            purifiedEcrit(result.pronom);
+                        }
+                    });
+
+
+                } else {
+
+                    purified(result.pronom);
+
+                    browser.storage.local.get("purification").then((res) => {
+                        if (res.purification == "on") {
+                            purifiedEcrit(result.pronom);
+                        }
+                    });
+
+
+                }
+
+
+            });
+
+
+        }
+
     });
-  }
+  }, time);
+}
 
-
-$(document).ready(function() {
-  
-browser.storage.local.get("protection").then((onouoff) => {
-  if(onouoff.protection == undefined){
-    
-    browser.storage.local.set({"protection": "off"});
-  
-  }else if (onouoff.protection == "on"){
-
-    browser.storage.local.get("pronom").then((result) => {
-      if (result.pronom == undefined){
-        browser.storage.local.set({"pronom": "il"});
-        purified("il");
-        console.log(result.pronom, result.value);
-      
-        browser.storage.local.get("purification").then((res) => {
-          if (res.purification == "on"){
-            purifiedEcrit(result.pronom);
-          }
-        });
-      
-      
-      
-      }else{
-
-        console.log(result.pronom);
-        purified(result.pronom);
-
-        browser.storage.local.get("purification").then((res) => {
-          if (res.purification == "on"){
-            purifiedEcrit(result.pronom);
-          }
-        });
-      
-      
-      
-      
-      }
-    
-    
-    
-    
-    
-    
-    });
-
-
-    
-
-  }
-
-});
-
-});
-
-
+document.addEventListener("DOMContentLoaded", allonsycamarades());
